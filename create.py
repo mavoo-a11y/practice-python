ade1decda2dbf6af635aa51ff76e728184e97265
mavoo-a11y <marvinlone456@gmail.com>
2025-03-26 13:39:52 +0300

    create.py

---
+++ create.py
@@ -0,0 +1,18 @@
+name = "Bro code"
+age = 25
+gpa = 3.2
+is_sudent = True
+# change to int 
+gpa = int(gpa)
+
+print(gpa)
+# change to string
+age = str(age)
+
+age += "1"
+
+print(age)
+#change to bool
+name = bool(name)
+
+print(name)
